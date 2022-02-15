d73d5ee6cf387bae5f1c75d712764347bd5f2932
rrerusu <rrerusu@gmail.com>
2022-02-15 12:46:42 -0600

    Int equivalents of chars

---
+++ chapter2/exercises/_0209/integer-equivalent-of-chars.py
@@ -0,0 +1,16 @@
+# exercise 02 - 09
+"""Determine the integer equivalents of characters"""
+
+print("Integer equivalent of B is {}".format(ord('B')))
+print("Integer equivalent of C is {}".format(ord('C')))
+print("Integer equivalent of D is {}".format(ord('D')))
+print("Integer equivalent of b is {}".format(ord('b')))
+print("Integer equivalent of c is {}".format(ord('c')))
+print("Integer equivalent of d is {}".format(ord('d')))
+print("Integer equivalent of 0 is {}".format(ord('0')))
+print("Integer equivalent of 1 is {}".format(ord('1')))
+print("Integer equivalent of 2 is {}".format(ord('2')))
+print("Integer equivalent of $ is {}".format(ord('$')))
+print("Integer equivalent of * is {}".format(ord('*')))
+print("Integer equivalent of + is {}".format(ord('+')))
+print("Integer equivalent of ' ' is {}".format(ord(' ')))
