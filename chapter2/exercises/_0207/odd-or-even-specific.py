56494f76b7589cce3a762a892db5c8315332b567
rrerusu <rrerusu@gmail.com>
2022-02-15 12:35:43 -0600

    Determine whether 1024 is divisible by 4 and whether 10 is divisible by 2

---
+++ chapter2/exercises/_0207/odd-or-even-specific.py
@@ -0,0 +1,10 @@
+# exercise 02 - 07
+"""Determine if 1024 is divisible by 4 and if 10 is divisible by 2"""
+
+print("1024 is {}divisible by 4".format(
+    "" if 1024 % 4 == 0 else "not "
+))
+
+print("10 is {}divisible by 2".format(
+    "" if 10 % 2 == 0 else "not "
+))
