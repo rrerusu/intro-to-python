7b273d35d97c9a591426789aaebc01f4f9a418fe
rrerusu <rrerusu@gmail.com>
2022-02-15 12:22:04 -0600

    Arithmetic operations on 27.5

---
+++ chapter2/exercises/_0204/arithmetic.py
@@ -0,0 +1,9 @@
+# exercise 02-04
+"""Print all arithmetic operations with 27.5 as first number and 2 as second number"""
+
+print("27.5 + 2 = " + str(27.5 + 2))
+print("27.5 - 2 = " + str(27.5 - 2))
+print("27.5 * 2 = " + str(27.5 * 2))
+print("27.5 / 2 = " + str(27.5 / 2))
+print("27.5 // 2 = " + str(27.5 // 2))
+print("27.5 ** 2 = " + str(27.5 ** 2))
