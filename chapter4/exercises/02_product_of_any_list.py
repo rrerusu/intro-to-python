7c0cc1b49909340243cfe7e4517fbfcffe925923
rrerusu <rrerusu@gmail.com>
2022-03-08 22:29:51 -0600

    Calculate the product of a list, create a bar chart using lists and for loops

--- chapter4/exercises/02_product_of_any_list.py
+++ chapter4/exercises/02_product_of_any_list.py
@@ -1,2 +1,19 @@
 # exercise 04 - 13
-''''''
+'''Calculate the product of all numbers in any list'''
+
+def calcProduct(nums):
+    '''Calculate product of values in a list'''
+    product = 1
+    for i in nums:
+        product *= i
+    return product
+
+myNums = []
+
+list_len = int(input("Enter length of the list: "))
+
+# Get values for array
+for i in range(list_len):
+    myNums += [int(input(f"Enter number {i + 1}: "))]
+
+print(f"Sum of numbers is: {calcProduct(myNums)}")
