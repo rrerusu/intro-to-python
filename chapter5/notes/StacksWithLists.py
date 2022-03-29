5cbdd0a558feea660e4756365b94f619bb6c41a1
rrerusu <rrerusu@gmail.com>
2022-03-29 14:38:54 -0500

    Simulating stacks with lists

---
+++ chapter5/notes/StacksWithLists.py
@@ -0,0 +1,14 @@
+stack = []
+stack.append('red')
+print("stack: {}".format(stack))
+
+stack.append('green')
+print("stack: {}".format(stack))
+
+print("stack.pop(): {}".format(stack.pop()))
+print("stack: {}".format(stack))
+
+print("stack.pop(): {}".format(stack.pop()))
+print("stack: {}".format(stack))
+
+print("stack: {}".format(stack))
